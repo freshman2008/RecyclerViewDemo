95f05d94aece5e2e84ebe6f81565ca83ba6bbb31
Li Xiuliang <xiuliang.li@gi-de.com>
2017-08-16 14:37:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gd001/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.gd001.recyclerviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(initDataset());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private List<String> initDataset(){
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<50; i++){
+            list.add("测试用例：" + i);
+        }
+        return list;
+    }
+}
